10030543fd40ee4dd18ba9f7536e0c07173b7963
audumus <audumusa233@gmail.com>
2023-02-16 23:22:12 -0800

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/
+/**
 *main -Entry point
 *Return: Always 0 (Success)
 */
@@ -10,6 +10,6 @@ int main(void)
         printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
         printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
         printf("Size of a float: %zu byte(s)\n", sizeof(float));
-                return (0);
+	return (0);
 }
 
